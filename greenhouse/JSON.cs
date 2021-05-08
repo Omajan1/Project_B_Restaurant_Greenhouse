887ddfbff1c055cf7e9b9c6863cf71e42dd946e8
Levidevtech <79833570+Levidevtech@users.noreply.github.com>
2021-05-08 19:58:54 +0200

    cas

---
+++ greenhouse/JSON.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace JSON
+{
+    public class paths
+    {
+        // Alle JSON patsh voor het makkelijk overnemen ervan
+        public static string vragen = @"../../../../data/vragen.json";
+        public static string reservaring = @"../../../../data/reservering.json";
+        public static string login = @"../../../../data/login.json";
+        public static string gerechten = @"../../../../data/Gerechten.json";
+    }
+}
+
