68f75296cc6a97e7cb9c3349b360e643c280ae5b
Levidevtech <79833570+Levidevtech@users.noreply.github.com>
2021-05-08 20:12:56 +0200

    Unit testing

--- UnitTesting/UnitTest1.cs
+++ UnitTesting/UnitTest1.cs
@@ -1,18 +1,42 @@
 using NUnit.Framework;
-
-namespace UnitTesting
+using Login;
+namespace Nunit3
 {
     public class Tests
     {
         [SetUp]
         public void Setup()
         {
+
         }
 
+
+
         [Test]
-        public void Test1()
+        public void Login_Geeft_False_Bij_Verkeerd_Wachtwoord_En_Username()
         {
-            Assert.Pass();
+            // Moet false worden
+            bool s = Login.Login.check("", "");
+
+            Assert.AreEqual(s, false);
         }
+
+
+
+
+        [Test]
+        public void Login_Geeft_True_Bij_Goed_Wachtwoord_En_Username()
+        {
+            // Moet false worden
+            bool s = Login.Login.check("123", "456");
+
+            Assert.AreEqual(s, true);
+        }
+
+
+
+
+
+
     }
 }
